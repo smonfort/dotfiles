943a9e54c2c52cc21259beb14d0a879bf17389ee
Stéphane <stephane.monfort@gmail.com>
2026-03-21 17:36:41 +0100

    fix(nvim): likec4 lsp

--- nvim/.config/nvim/lua/smonfort/plugins/likec4.lua
+++ nvim/.config/nvim/lua/smonfort/plugins/likec4.lua
@@ -1,5 +1,6 @@
 -- nvim likec4 lsp config/ syntax/ ftdetect : https://github.com/likec4/likec4.nvim
 return {
-	"likec4/likec4.nvim",
-	build = "npm install -g @likec4/language-server",
+	"smonfort/likec4.nvim",
+	branch = "fix/lsp-package-deprecation",
+	build = "npm install -g likec4",
 }
