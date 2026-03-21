b0556e17c5c9c2394ce7bb256997234171fb285a
Stéphane <stephane.monfort@gmail.com>
2026-03-21 17:57:12 +0100

    fix(nvim): todo comments

--- nvim/.config/nvim/lua/smonfort/plugins/todo-comments.lua
+++ nvim/.config/nvim/lua/smonfort/plugins/todo-comments.lua
@@ -1,10 +1,6 @@
 -- ✅ Highlight, list and search todo comments in your projects
 return {
 	"folke/todo-comments.nvim",
-	event = { "BufReadPre", "BufNewFile" },
 	dependencies = { "nvim-lua/plenary.nvim" },
-	config = function()
-		local todo_comments = require("todo-comments")
-		todo_comments.setup()
-	end,
+	opts = {},
 }
