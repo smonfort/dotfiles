4a1984354ed5939c9f9970c3a4002b6866142e6c
Stéphane <stephane.monfort@gmail.com>
2026-03-21 17:36:27 +0100

    feat(nvim): lsp config rework

---
+++ nvim/.config/nvim/lua/smonfort/plugins/lsp-config.lua
@@ -0,0 +1,8 @@
+return {
+	"neovim/nvim-lspconfig",
+	config = function()
+		-- vim.lsp.config("likec4", {
+		-- 	cmd = { "pnpm", "dlx", "likec4", "lsp", "--stdio" }, -- override likec4 lsp
+		-- })
+	end,
+}
