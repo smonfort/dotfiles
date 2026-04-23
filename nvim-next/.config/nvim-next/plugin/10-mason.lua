cf16ca354431733c2cd60866b2b75891f67069a0
Stéphane <stephane.monfort@gmail.com>
2026-04-23 20:59:36 +0200

    feat(nvim): do not use nvim-lspconfig

--- nvim-next/.config/nvim-next/plugin/10-mason.lua
+++ nvim-next/.config/nvim-next/plugin/10-mason.lua
@@ -1,12 +1,14 @@
 vim.pack.add({
 	gh("mason-org/mason.nvim"),
-	gh("neovim/nvim-lspconfig"),
+	-- gh("neovim/nvim-lspconfig"),
 	gh("mason-org/mason-lspconfig.nvim"),
 	gh("https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim"),
 })
 
 require("mason").setup({})
-require("mason-lspconfig").setup()
+require("mason-lspconfig").setup({
+	automatic_enable = false,
+})
 require("mason-tool-installer").setup({
 	ensure_installed = {
 		"lua_ls",
