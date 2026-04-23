1728f4a793f46ae77c535b4982e6d72c96c2ba2a
Stéphane <stephane.monfort@gmail.com>
2026-04-23 20:58:41 +0200

    feat(nvim): configure ts_ls

---
+++ nvim-next/.config/nvim-next/lsp/ts_ls.lua
@@ -0,0 +1,20 @@
+---@type vim.lsp.Config
+return {
+	cmd = { "typescript-language-server", "--stdio" },
+	filetypes = {
+		"javascript",
+		"javascriptreact",
+		"javascript.jsx",
+		"typescript",
+		"typescriptreact",
+		"typescript.tsx",
+	},
+	root_markers = { "package.json" },
+	init_options = {
+		preferences = {
+			includeCompletionsForModuleExports = true,
+			includeCompletionsForImportStatements = true,
+			importModuleSpecifierPreference = "non-relative",
+		},
+	},
+}
