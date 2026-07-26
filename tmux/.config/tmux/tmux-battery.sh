b2ea026732210d7cc51705606c6d74e55aedc436
Stéphane <stephane.monfort@gmail.com>
2026-07-26 21:22:10 +0200

    feat(tmux): change battery level color

--- tmux/.config/tmux/tmux-battery.sh
+++ tmux/.config/tmux/tmux-battery.sh
@@ -1,6 +1,16 @@
 battery_info=$(pmset -g batt)
 
-percent=$(echo $battery_info | egrep -Eo '\d+%')
-charging_indicator=$(echo $battery_info | grep -q discharging && echo "🔋" || echo "⚡")
+percent=$(echo "$battery_info" | egrep -Eo '\d+%' | head -1 | tr -d '%')
+charging_indicator=$(echo "$battery_info" | grep -q discharging && echo "🔋" || echo "⚡")
 
-echo $charging_indicator$percent
+if [ "$percent" -gt 75 ]; then
+    color="green"
+elif [ "$percent" -ge 50 ]; then
+    color="yellow"
+elif [ "$percent" -ge 25 ]; then
+    color="colour208"
+else
+    color="red"
+fi
+
+echo "#[fg=${color}]${charging_indicator}${percent}%#[fg=default]"
