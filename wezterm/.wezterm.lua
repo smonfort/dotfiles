6b5f53a4c4a896c0de29464593ce0183bde59a71
Stéphane <stephane.monfort@gmail.com>
2026-07-26 22:50:40 +0200

    feat(wezterm): add nerd font

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -2,6 +2,7 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.hide_tab_bar_if_only_one_tab = true
+config.font = wezterm.font("JetBrainsMono Nerd Font Mono")
 config.font_size = 14
 
 -- Required for neovim zen-mode : https://github.com/folke/zen-mode.nvim?tab=readme-ov-file#wezterm
