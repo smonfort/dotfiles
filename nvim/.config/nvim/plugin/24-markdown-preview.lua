74834352f48a767ee4f96ed10e45028414f10cd1
Stéphane Monfort <stephane.monfort@huwise.com>
2026-08-07 11:31:52 +0200

    feat(nvim): add markdown preview plugin

---
+++ nvim/.config/nvim/plugin/24-markdown-preview.lua
@@ -0,0 +1,17 @@
+-- Build hook must be registered before vim.pack.add() to catch the install event
+vim.api.nvim_create_autocmd("PackChanged", {
+	callback = function(ev)
+		if ev.data.spec.name == "markdown-preview.nvim" and (ev.data.kind == "install" or ev.data.kind == "update") then
+			if not ev.data.active then
+				vim.cmd.packadd("markdown-preview.nvim")
+			end
+			vim.fn["mkdp#util#install"]()
+		end
+	end,
+})
+
+vim.pack.add({
+	gh("iamcco/markdown-preview.nvim"),
+})
+
+vim.keymap.set("n", "<leader>mp", "<cmd>MarkdownPreviewToggle<cr>", { desc = "Toggle markdown preview" })
