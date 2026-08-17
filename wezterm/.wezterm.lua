8bd386a192c64278ba8e8a7aa9193b9d07bbab19
Stéphane <stephane.monfort@gmail.com>
2026-08-17 21:52:46 +0200

    feat(wezterm): set window decoration to RESIZE

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -4,5 +4,6 @@ local config = wezterm.config_builder()
 config.hide_tab_bar_if_only_one_tab = true
 config.font = wezterm.font("JetBrainsMono Nerd Font Mono")
 config.font_size = 14
+config.window_decorations = "RESIZE"
 
 return config
