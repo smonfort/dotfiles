8cdf8d9eb4d76b116adf6ef621d1afcb43824e5a
Stéphane <stephane.monfort@gmail.com>
2026-08-31 00:03:35 +0200

    feat(sketchybar): change active screen color

--- sketchybar/.config/sketchybar/plugins/monitor_focus.sh
+++ sketchybar/.config/sketchybar/plugins/monitor_focus.sh
@@ -6,7 +6,7 @@ MONITOR_ID="$1"
 FOCUSED_MONITOR="$(aerospace list-monitors --focused --format "%{monitor-id}")"
 
 if [ "$MONITOR_ID" = "$FOCUSED_MONITOR" ]; then
-    sketchybar --set "$NAME" background.drawing=off icon.color=$MUTED_ORANGE
+    sketchybar --set "$NAME" background.drawing=off icon.color=$ORANGE
 else
     sketchybar --set "$NAME" background.drawing=off icon.color=$COMMENT
 fi
