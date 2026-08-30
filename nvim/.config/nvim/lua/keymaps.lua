fcbf24972f54ba5cdbaf481e331eb6c3689b84b2
Stéphane <stephane.monfort@gmail.com>
2026-08-30 22:23:12 +0200

    feat(nvim): add leader + pu to update packages

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -7,3 +7,8 @@ vim.keymap.set({ "n", "i", "v" }, "<left>", '<cmd>echo "Use h to move!!"<CR>')
 vim.keymap.set({ "n", "i", "v" }, "<right>", '<cmd>echo "Use l to move!!"<CR>')
 vim.keymap.set({ "n", "i", "v" }, "<up>", '<cmd>echo "Use k to move!!"<CR>')
 vim.keymap.set({ "n", "i", "v" }, "<down>", '<cmd>echo "Use j to move!!"<CR>')
+
+-- update all plugins installed via the native package manager (vim.pack, nvim 0.12+)
+vim.keymap.set("n", "<leader>pu", function()
+	vim.pack.update()
+end, { desc = "Update plugins" })
